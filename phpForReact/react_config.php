dfe141c3cb90615a98179bce8af0a1e283f2e247
DominikHupko <hupkodominik143@gmail.com>
2025-05-07 13:03:45 +0200

    small fix

--- phpForReact/react_config.php
+++ phpForReact/react_config.php
@@ -12,7 +12,7 @@ try {
     $host = $_ENV['DB_HOST'];
     $dbname = $_ENV['DB_NAME'];
     $user = $_ENV['DB_USER'];
-    $pass = $_ENV['DB_PASS'];
+    $pass = $_ENV['DB_PASSWORD'];
 
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
